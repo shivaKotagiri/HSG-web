bd69436a916f9fa27cfeaf51cd6fc4d0b01cabf0
helpckrdatapoint-blip <help.ckrdatapoint@gmail.com>
2025-12-15 22:17:47 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,13 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      // Added configuration for i.ibb.co
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
